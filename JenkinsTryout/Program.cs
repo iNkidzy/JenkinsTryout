d13850fc3f9b86e1e82bd86cca14d954ff5a1593
Nadia Miteva <nadq.nadia@gmail.com>
2021-02-05 12:53:43 +0100

    init commit

---
+++ JenkinsTryout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsTryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
